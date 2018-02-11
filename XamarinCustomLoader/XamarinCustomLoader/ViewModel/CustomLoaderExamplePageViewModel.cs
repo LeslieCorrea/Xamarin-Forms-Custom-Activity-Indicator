36dbb6d3256de3c414ae77c71495905bfcdda5bf
LESLIE CORREA <correaleslie69@yahoo.com>
2018-02-11 22:02:33 +0530

    Added Command commit

--- XamarinCustomLoader/XamarinCustomLoader/ViewModel/CustomLoaderExamplePageViewModel.cs
+++ XamarinCustomLoader/XamarinCustomLoader/ViewModel/CustomLoaderExamplePageViewModel.cs
@@ -1,6 +1,28 @@
-﻿namespace XamarinCustomLoader.ViewModel
+﻿using Xamarin.Forms;
+using MvvmHelpers;
+using System.Threading.Tasks;
+using System;
+using System.Windows.Input;
+
+namespace XamarinCustomLoader.ViewModel
 {
-    public class CustomLoaderExamplePageViewModel
+    public class CustomLoaderExamplePageViewModel : BaseViewModel
     {
+        public ICommand ButtonClicked { get; }
+
+        public CustomLoaderExamplePageViewModel()
+        {
+            ButtonClicked = new Command(async () => await ShowLoader());
+        }
+
+        private async Task ShowLoader()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+            await Task.Delay(1000);
+            IsBusy = false;
+        }
     }
 }
